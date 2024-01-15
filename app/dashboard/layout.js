762f9f16a2d9639f21116e4ec9d747bb6aa6d60f
Kingsley <ekehkingsley@yahoo.com>
2024-01-15 19:31:05 +0100

    new comm

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,7 +1,7 @@
 import Navbar from "../ui/dashboard/navbar/navbar";
 import Sidebar from "../ui/dashboard/sidebar/sidebar";
 import styles from "../ui/dashboard/dashboard.module.css";
-import Footer from "../ui/dashboard/footer/Footer.js";
+import Footer from "../ui/dashboard/footer/Footer";
 
 const Layout = ({ children }) => {
   return (
