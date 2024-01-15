485b2ccc07d0b04fba7dbe4393ee19c8c5ec6d1d
Kingsley <ekehkingsley@yahoo.com>
2024-01-15 19:22:43 +0100

    new commit

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,9 @@ import styles from "./ui/login/login.module.css";
 const Homepage = () => {
   return (
     <div className={styles.container2}>
-      <h2>HOME PAGE</h2>
+      <Link href={"/dashboard/users"}>
+        <h2>HOME PAGE</h2>
+      </Link>
 
       <Link href={"/dashboard"}>
         <button className={styles.btn}>go to admin</button>
